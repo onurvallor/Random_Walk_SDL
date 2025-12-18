5e64e586bd37c977d53d9fd376a571932d8d5eb9
Onur Vallor <onurvallor@gmail.com>
2025-12-18 02:17:57 -0500

    Replace SDL Surface with SDL Renderer to be able to draw. Added event polling to capture keyboard key-press.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_keyboard.h>
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
@@ -22,9 +23,9 @@ void close();
 
 SDL_Window *gWindow{nullptr};
 
-SDL_Surface *gScreenSurface{nullptr};
+// SDL_Surface *gScreenSurface{nullptr};
 
-SDL_Surface *gHelloWorld{nullptr};
+// SDL_Surface *gHelloWorld{nullptr};
 
 SDL_Renderer *gRenderer{nullptr};
 
@@ -40,38 +41,25 @@ bool init() {
         gWindow == nullptr) {
       SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
       success = false;
-    } else {
-      gScreenSurface = SDL_GetWindowSurface(gWindow);
+      //} else {
+      // gScreenSurface = SDL_GetWindowSurface(gWindow);
+      // gRenderer = SDL_GetRenderer(gWindow);
     }
     if (gRenderer = SDL_CreateRenderer(gWindow, nullptr);
         gRenderer == nullptr) {
       SDL_Log("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
       success = false;
+    } else {
+      gRenderer = SDL_GetRenderer(gWindow);
     }
   }
   return success;
 }
 
-bool loadMedia() {
-  bool success{true};
-
-  std::string imagePath{"./snail.bmp"};
-
-  if (gHelloWorld = SDL_LoadBMP(imagePath.c_str()); gHelloWorld == nullptr) {
-    SDL_Log("Unable to load image %s! SDL Error: %s\n", imagePath.c_str(),
-            SDL_GetError());
-    success = false;
-  }
-  return success;
-}
-
 void close() {
-  SDL_DestroySurface(gHelloWorld);
-  gHelloWorld = nullptr;
 
   SDL_DestroyWindow(gWindow);
   gWindow = nullptr;
-  gScreenSurface = nullptr;
 
   SDL_DestroyRenderer(gRenderer);
   gRenderer = nullptr;
@@ -86,37 +74,27 @@ int main() {
     SDL_Log("Unable to initialize program!\n");
     exitCode = 1;
   } else {
-    if (loadMedia() == false) {
-      SDL_Log("Unable to load media!\n");
-      exitCode = 2;
-    } else {
-      bool quit{false};
+    SDL_Event e{};
+    bool isRunning{1};
 
-      SDL_Event e;
-      SDL_zero(e);
+    while (isRunning) {
+      while (SDL_PollEvent(&e)) {
 
-      while (quit == false) {
-        while (SDL_PollEvent(&e) == true) {
-          if (e.type == SDL_EVENT_QUIT) {
-            quit = true;
-          }
+        if (e.key.scancode == SDL_SCANCODE_Q) {
+          isRunning = 0;
         }
-        SDL_FillSurfaceRect(
-            gScreenSurface, nullptr,
-            SDL_MapSurfaceRGB(gScreenSurface, 0xFF, 0xFF, 0xFF));
-
-        SDL_BlitSurface(gHelloWorld, nullptr, gScreenSurface, nullptr);
-
-        SDL_SetRenderDrawColor(gRenderer, 0xF0, 0xF0, 0xF0, 0xFF);
-        SDL_RenderLine(gRenderer, 20.0f, 20.0f, 40.0f, 70.0f);
-
-        SDL_UpdateWindowSurface(gWindow);
       }
-    }
-    close();
+      SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+      SDL_RenderClear(gRenderer);
 
-    return exitCode;
+      SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 0);
+      SDL_RenderLine(gRenderer, 40.0f, 40.0f, 60.0f, 85.0f);
+
+      SDL_RenderPresent(gRenderer);
+    }
   }
 
-  return 0;
+  close();
+
+  return exitCode;
 }
